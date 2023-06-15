4407014636f1856e183f987798d2663303ac5ffc
Abhishek taur <122015066+abhitaur@users.noreply.github.com>
2023-06-16 01:36:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 //import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
-//import Navbar from './Components/Navbar'
-//import Products from './Components/Products'
-//import SingleProductpage from './Components/Singleproductpage'
-//import Shoes from './Components/Shoes'
+import Navbar from './Components/Navbar'
+import Products from './Components/Products'
+import SingleProductpage from './Components/Singleproductpage'
+import Shoes from './Components/Shoes'
 import AddtoCart from './Components/AddtoCart'
 export default function App() {
   return (
     <> 
-
+<Navbar />
+    <Products />
+    <SingleProductpage/>
+    <Shoes  />
+    <AddtoCart />
+    
 
 
 
